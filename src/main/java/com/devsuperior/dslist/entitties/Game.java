635b69fb5a21f684b4afebb71a2f8455bba3dc9a
michel_mhl <michel_mhl@hotmail.com>
2023-05-08 21:53:19 -0300

    Game seed - inserindo manualmente os dados

--- src/main/java/com/devsuperior/dslist/entitties/Game.java
+++ src/main/java/com/devsuperior/dslist/entitties/Game.java
@@ -14,9 +14,12 @@ public class Game {
    @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+   @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Long getId() {
@@ -43,6 +46,14 @@ public class Game {
         this.year = year;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     public String getGenre() {
         return genre;
     }
@@ -51,12 +62,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlataforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlataform(String plataforms) {
+        this.platforms = plataforms;
     }
 
     public String getImgUrl() {
@@ -96,15 +107,17 @@ public class Game {
         return Objects.hash(id);
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+
     }
 
     public Game() {
